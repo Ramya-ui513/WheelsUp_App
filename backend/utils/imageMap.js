59894a01647384e7dc523dc060dc459cc7fd7c73
Ramya-ui513 <ramyasai1999b@gmail.com>
2025-07-08 22:49:31 +0100

    uploaded assets and utils

---
+++ backend/utils/imageMap.js
@@ -0,0 +1,30 @@
+// frontend/utils/imageMap.js
+import BeastCrusher from '../assets/Beast Crusher XL – Monster Truck.jpg';
+import BlazeMonster from '../assets/Blaze Monster X – Monster Truck.jpg';
+import CircuitBullet from '../assets/Circuit Bullet – Track Champion.jpg';
+import DesertRover from '../assets/Desert Rover Pro – Off-Road.jpg';
+import HyperDrifter from '../assets/Hyper Drifter – Street Racer.jpg';
+import MegaClimb from '../assets/Mega Climb King – Monster Truck.jpg';
+import NitroFury from '../assets/Nitro FURY GT – Street Racer.jpg';
+import SandDigger from '../assets/Sand Digger – Off-Road.jpg';
+import StreetHawk from '../assets/Street Hawk RX – Street Racer.jpg';
+import TrackBlazer from '../assets/Track Blazer R1 – Track Champion.jpg';
+import TrackPhantom from '../assets/Track Phantom – Track Champion.jpg';
+import TurboVortex from '../assets/Turbo Vortex Z9 – Street Racer.jpg';
+
+const imageMap = {
+  BeastCrusher,
+  BlazeMonster,
+  CircuitBullet,
+  DesertRover,
+  HyperDrifter,
+  MegaClimb,
+  NitroFury,
+  SandDigger,
+  StreetHawk,
+  TrackBlazer,
+  TrackPhantom,
+  TurboVortex,
+};
+
+export default imageMap;
