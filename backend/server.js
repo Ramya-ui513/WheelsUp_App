c8c1543d514fe03e358b7a90c87940e5bc419d08
Ramya-ui513 <ramyasai1999b@gmail.com>
2025-08-04 16:51:16 +0100

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,9 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.js';
-
-
+import cartRoutes from './routes/cart.js';
+import addressRoutes from './routes/address.js';
+import emailRoutes from './routes/email.js';
 import productRoutes from './routes/products.js';
 
 dotenv.config();
@@ -28,11 +29,6 @@ mongoose.connect(process.env.MONGO_URI, {
 .catch((err) => console.error('MongoDB connection error:', err));
 
 app.use('/api/products', productRoutes);
-
-import cartRoutes from './routes/cart.js';
 app.use('/api/cart', cartRoutes);
-import addressRoutes from './routes/address.js';
-
 app.use('/api/address', addressRoutes); 
-import emailRoutes from './routes/email.js';
 app.use('/api/email', emailRoutes);
